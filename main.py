ba8d0daf008c6c41fcc7d22251f19aa4e3bb7a64
alialiearan-cell <ali.aliearan@gmail.com>
2025-09-08 23:53:39 +0330

    Update_wait_to_thread main.py

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ from time import sleep
 a = 0
 while True:
     threading.Thread(target=test.tarrer,args=("104.18.147.40",)).start()
-    threading.Thread(target=test.tarrer,args=("104.18.146.40",)).start()
+    th = threading.Thread(target=test.tarrer,args=("104.18.146.40",))
+    th.start()
     sleep(uniform(1,2.5))
     if a == 100:
         break
-    a += 1 
+    a += 1
+th.join()
 print(f"finish all code\nsended packet:{test.i}")
+
 
